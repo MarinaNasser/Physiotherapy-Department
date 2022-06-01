c604de3160a601bba357a13b8bd35c90157ac61c
alaayasser01 <alaayasser499@gmail.com>
2022-06-01 23:28:07 +0200

    error

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ app.secret_key = "very secret key"
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="Ahmed9112",
+    passwd="A_0l1a2a3",
     database="felcode"
 )
 mycursor = mydb.cursor(buffered=True)
