94f7df10363ff0f2ddf75129c1044faa3a6ba47f
Ahmed Emad Eldeen Ahmed <ahmedemad9112@gmail.com>
2022-06-02 15:18:36 +0200

    please make sure u accept all incoming 34an ana t3bt
    ya gm3a kfaya delete ll7aga argoko accept el incoming argookoom

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ app.secret_key = "very secret key"
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="magdynasr",
+    passwd="Ahmed9112",
     database="felcode"
 )
 mycursor = mydb.cursor(buffered=True)
